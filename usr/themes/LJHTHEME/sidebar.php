16a2fa443704c5890bcba31003ee56c685c444e8
Li Jianhua <lijianhua_oop@163.com>
2015-04-01 10:51:18 +0800

    video width

--- usr/themes/LJHTHEME/sidebar.php
+++ usr/themes/LJHTHEME/sidebar.php
@@ -4,17 +4,17 @@
             <dt>文章</dt>
             <?php $this->widget('Widget_Contents_Post_Recent')->parse('<dd><a href="{permalink}" class="item" target="_blank">{title}</a></dd>'); ?>
         </dl>
-        <dl>
-            <dt>项目</dt>
-            <dd><a href="#">Android 开发的昨天、今天和明天</a> </dd>
-            <dd><a href="#">迁移到了 Digital Ocean 的新加坡节点</a> </dd>
-            <dd><a href="#">升级和改造 NAS 小记</a> </dd>
-            <dd><a href="#">更换倒车摄像头小记</a> </dd>
-            <dd><a href="#">WiFi-Duino 使用小结</a> </dd>
-            <dd><a href="#">给女儿的一封信</a> </dd>
-            <dd><a href="#">我常用的 Mac 应用（2014）</a> </dd>
-            <dd><a href="#">我常用的 iOS 应用和插件（2014版）</a> </dd>
-        </dl>
+        <!-- <dl> -->
+        <!--     <dt>项目</dt> -->
+        <!--     <dd><a href="#">Android 开发的昨天、今天和明天</a> </dd> -->
+        <!--     <dd><a href="#">迁移到了 Digital Ocean 的新加坡节点</a> </dd> -->
+        <!--     <dd><a href="#">升级和改造 NAS 小记</a> </dd> -->
+        <!--     <dd><a href="#">更换倒车摄像头小记</a> </dd> -->
+        <!--     <dd><a href="#">WiFi&#45;Duino 使用小结</a> </dd> -->
+        <!--     <dd><a href="#">给女儿的一封信</a> </dd> -->
+        <!--     <dd><a href="#">我常用的 Mac 应用（2014）</a> </dd> -->
+        <!--     <dd><a href="#">我常用的 iOS 应用和插件（2014版）</a> </dd> -->
+        <!-- </dl> -->
         <dl>
             <dt>分类</dt>
         <?php $this->widget('Widget_Metas_Category_List')->listCategories(); ?>
